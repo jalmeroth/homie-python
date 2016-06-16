1f9ddb74efa1d8d9340e1c4e72c5d25d5979bfba
jalmeroth <jan@almeroth.com>
2016-06-16 17:18:04 +0200

    Update to examples

--- relay_switch.py
+++ relay_switch.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+Homie = homie.Homie(**homie.config)
 switchNode = homie.HomieNode("switch", "switch")
 
 
@@ -12,12 +13,13 @@ def switchOnHandler(mqttc, obj, msg):
     payload = msg.payload.decode("UTF-8").lower()
     if payload == 'true':
         logger.info("Switch: ON")
+        Homie.setNodeProperty(switchNode, "on", "true", True)
     else:
         logger.info("Switch: OFF")
+        Homie.setNodeProperty(switchNode, "on", "false", True)
 
 
 def main():
-    Homie = homie.Homie(**homie.config)
     Homie.setFirmware("relay-switch", "1.0.0")
     Homie.subscribe(switchNode, "on", switchOnHandler)
 
--- temperature_sensor.py
+++ temperature_sensor.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TEMPERATURE_INTERVAL = 0.3
+TEMPERATURE_INTERVAL = 3
 
 temperatureNode = homie.HomieNode("temperature", "temperature")
 
