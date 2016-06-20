ade811247014a8a4d32c6efdb32b8c4283f9d4e5
jalmeroth <jan@almeroth.com>
2016-06-20 12:30:01 +0200

    Release 0.1.3 found on PyPI

---
+++ README.txt
@@ -0,0 +1,3 @@
+This repo is work in progress.
+
+Experimental implementation of the IoT convention called homie (https://github.com/marvinroger/homie) in Python.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name="homie",
+    packages=["homie"],
+    version="0.1.3",
+    description="Experimental implementation of the IoT convention called homie",
+    author="Jan Almeroth",
+    author_email="homie-python@almeroth.com",
+    url="https://github.com/jalmeroth/homie-python",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation"
+    ]
+)
