f760a94478c503ace940c1c0e0c5245f07ad840c
jalmeroth <jan@almeroth.com>
2016-06-20 21:07:59 +0200

    Switching to setuptools, requires paho-mqtt

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="homie",
     packages=["homie"],
-    version="0.1.3",
+    version="0.1.4",
     description="Experimental implementation of the IoT convention called homie",
     author="Jan Almeroth",
     author_email="homie-python@almeroth.com",
@@ -15,5 +15,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
         "Topic :: Home Automation"
-    ]
+    ],
+    install_requires=['paho-mqtt>=1.2']
 )
