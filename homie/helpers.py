f93167e5da3d8db8331253909333b2a1004b9ab4
Jan Almeroth <jan@5343cd7a5eae.fritz.box>
2018-05-14 21:16:54 +0200

    Fix pydocstyles

--- homie/helpers.py
+++ homie/helpers.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Provide helper functions."""
 import re
 import logging
 from uuid import getnode as get_mac
@@ -7,20 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 def generateDeviceId():
+    """Generate device Id."""
     logger.debug("generateDeviceId")
     return "{:02x}".format(get_mac())
 
 
 def isIdFormat(idString):
+    """Validate device Id."""
     logger.debug("isIdFormat")
     if isinstance(idString, str):
         r = re.compile('(^(?!\-)[a-z0-9\-]+(?<!\-)$)')
         return True if r.match(idString) else False
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
